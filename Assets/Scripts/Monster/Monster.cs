66891f6c4647d52c085877cde44ad5cb0fb3fe96
nyj504 <dydwn980511@gmail.com>
2025-04-16 00:16:39 +0900

    0416 나용주
    합치기

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -2,29 +2,26 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-    public Transform Player;
+    private Player _player;
     [SerializeField]
     private float _speed = 3.0f;
     [SerializeField]
     private float _stopDistance = 1.0f;
 
+    private void Start()
+    {
+        _player = Player.Instance;
+    }
     void Update()
     {
-        Trace();
+        float _distance = Vector3.Distance(transform.position, _player.transform.position);
+
+        Vector3 _direction = (_player.transform.position - transform.position).normalized;
+        transform.Translate(_direction * _speed * Time.deltaTime);
+
+        Quaternion lookRotation = Quaternion.LookRotation(_direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 
-    private void Trace()
-    {
-        float _distance = Vector3.Distance(transform.position, Player.transform.position);
-
-        if (_distance > _stopDistance)
-        {
-            Vector3 _direction = (Player.position - transform.position).normalized;
-            transform.position += _direction * _speed * Time.deltaTime;
-
-            Quaternion lookRotation = Quaternion.LookRotation(_direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
-        }
-    }
 
 }
