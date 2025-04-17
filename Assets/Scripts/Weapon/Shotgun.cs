8c489fc18b8acd37e96b62f2b5f3478867341ad3
Useop517 <bong44517@gmail.com>
2025-04-18 01:04:00 +0900

    250418 송유섭
    샷건 이펙트 실패

--- Assets/Scripts/Weapon/Shotgun.cs
+++ Assets/Scripts/Weapon/Shotgun.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 
-public class Shotgun : MonoBehaviour
+public class Shotgun : Weapon
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        base.Start();
+
+        SetSkill(205);
+        SetSkill(206);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void UseQSkill()
     {
-        
+         Vector3 offset = _player.transform.forward * 1.2f + _player.transform.up * 1.0f;
+        PlayEffectAttachedToPlayer(_qSkillPrefab, _player.transform.position + offset);
+    }
+
+    public override void UseESkill()
+    {
+        Vector3 offset = _player.transform.forward * 1.2f + _player.transform.up * 1.0f;
+        PlayEffectAttachedToPlayer(_qSkillPrefab, _player.transform.position + offset);
     }
 }
